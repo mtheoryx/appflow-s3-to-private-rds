ebf68b72cb57cb1a6d76c363a61ef087a343f582
David Poindexter <drpoindexter@gmail.com>
2023-10-19 12:38:37 -0400

    Create a new vpc, with public and private subnet

--- lib/s3-to-rds-stack.ts
+++ lib/s3-to-rds-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { Construct } from "constructs";
 
 export class S3ToRdsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a new VPC
+    const vpc = new ec2.Vpc(this, "VPC", {
+      vpcName: "s3-to-rds-vpc",
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
+      natGateways: 0,
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          name: "s3-to-rds-public-subnet-1",
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: "s3-to-rds-isolated-subnet-1",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 28,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3ToRdsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create a security group for the ec2 instance (bastion)
+
+    // Create the ec2 Instance
   }
 }
